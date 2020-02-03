59fb59d0c5e92dc2c5710c081f3224176eed29a5
Matt Oberle <matt.r.oberle@gmail.com>
2020-02-03 14:32:43 -0500

    purges via 'PURGE' HTTP method when FASTLY_API_KEY is not provided
    Passing a FASTLY_API_KEY to the browser is not an especially secure
    action. The ideal way to provide a purging service would be to hide the
    Fastly calls behind an Amphora route (relying on the Amphora
    authentication).
    
    In lieu of that, if an IP origin list is used to secure PURGE requests,
    we can skip the API key entirely. This situation would apply if editing
    is restricted to users on an internal network or VPN.

--- src/utils/purge.js
+++ src/utils/purge.js
@@ -3,12 +3,30 @@
 const { FASTLY_API_URL } = require('./constants');
 
 /**
- * Make a purge request to Fastly
+ * Make a purge request to Fastly via simple PURGE HTTP method.
  * @param {string} url
  * @returns {Promise<Object>} An object containing the request data
  * @throws {Error} Throws an error if the request was not successful
  */
-module.exports = url => {
+const purgeUnauthenticated = function (url) {
+  return fetch(url, { method: 'PURGE' })
+    .then(res => res.json())
+    .then(res => {
+      res.url = url;
+      return res;
+    })
+    .catch(e => {
+      throw e;
+    });
+};
+
+/**
+ * Make a purge request to Fastly via authenticated API.
+ * @param {string} url
+ * @returns {Promise<Object>} An object containing the request data
+ * @throws {Error} Throws an error if the request was not successful
+ */
+const purgeAuthenticated = function (url) {
   return fetch(`${FASTLY_API_URL}/${url}`, {
     method: 'POST',
     headers: {
@@ -24,3 +42,19 @@ module.exports = url => {
       throw e;
     });
 };
+
+/**
+ * Generic entrypoint for authenticated/unauthenticated purge reuqests.
+ * @param {string} url
+ * @returns {Promise<Object>} An object containing the request data
+ * @throws {Error} Throws an error if the request was not successful
+ */
+const purge = function (url) {
+  return (
+    window.process.env.FASTLY_API_KEY
+    ? purgeAuthenticated(url)
+    : purgeUnauthenticated(url)
+  );
+}
+
+module.exports = purge;
